36b7b90e97091dd6d03b599721f2ce66f28525fb
Gabriel Zayas <g-zayas@hotmail.com>
2022-05-02 19:18:15 +0900

    First commit

---
+++ lib/bullet_train/release.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "release/version"
+
+module BulletTrain
+  module Release
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
